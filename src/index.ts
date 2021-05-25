ee6c07d83ebca5cc7d72c10980df4201fb26c540
takano <ri1saxxminny83xx@gmail.com>
2021-05-25 23:42:52 +0900

    function

--- src/index.ts
+++ src/index.ts
@@ -5,9 +5,20 @@
 // world.sayHello(root)
 
 // 03.基本の型定義
-import { anySample, notExistSample, primitiveSample, unknownSample } from './basic'
+// import { anySample, notExistSample, primitiveSample, unknownSample } from './basic'
+//
+// primitiveSample()
+// notExistSample()
+// anySample()
+// unknownSample()
 
-primitiveSample()
-notExistSample()
-anySample()
-unknownSample()
+// 04.関数の型定義
+import { logMessage7 } from './function/basic'
+import { isUserSignedIn, isUserSignedIn2, sumProductsPrice } from './function/parameters'
+
+logMessage7('Hello TypeScript')
+isUserSignedIn('ABC', 'tora')
+isUserSignedIn('DEF')
+isUserSignedIn2('ABC')
+const sum = sumProductsPrice(100, 200, 400, 500)
+console.log('Function parameters sample 5: Total price:', sum)
